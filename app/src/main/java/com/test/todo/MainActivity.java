70cad169570c67b2baf54cff44b78678a7814e93
Mukul Kumar <mukul.2121@gmail.com>
2020-06-18 11:04:51 +0530

    added splash screen and icons

--- app/src/main/java/com/test/todo/MainActivity.java
+++ app/src/main/java/com/test/todo/MainActivity.java
@@ -1,14 +1,41 @@
 package com.test.todo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView ic_logo;
+    MainActivity activity;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity = this;
+        ic_logo = findViewById(R.id.ic_logo);
+
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_in));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ic_logo.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.splash_out));
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ic_logo.setVisibility(View.GONE);
+                        startActivity(new Intent(activity, DashboardActivity.class));
+                        finish();
+                    }
+                }, 500);
+            }
+        }, 1500);
     }
 }
